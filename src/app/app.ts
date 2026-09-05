55353a96cb631a3f8db326e3a57d71f2c659d97b
Madan.patakota <madan.patakota@gmail.com>
2026-09-05 09:21:10 +0530

    INitianlsetup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('Step3_AngularAdvancedComponents_MDInterns');
+  
 }
