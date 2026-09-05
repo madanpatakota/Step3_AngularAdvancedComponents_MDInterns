c4cfd461b819667401603d9b0f13907f5559ebd0
Madan.patakota <madan.patakota@gmail.com>
2026-09-05 09:42:44 +0530

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { Employee } from './employee/employee';
 
 @Component({
-  imports: [],
+  imports: [Employee],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
---
+++ src/app/employee/employee.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-employee',
+  styleUrl: './employee.css',
+  templateUrl: './employee.html',
+})
+export class Employee {}
