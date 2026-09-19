6807b4449d0c23eccad4eb98ab588b001674ea2f
Madan.patakota <madan.patakota@gmail.com>
2026-09-19 12:05:24 +0530

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, HostListener } from '@angular/core';
 
 @Component({
   imports: [],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
+  // host: 
+  // {
+  //   '[style.color]'      : 'fontcolor',
+  //   '[style.font-weight]': 'fWeight',
+  //   '[style.background-color]': 'backgroundColor123444',
+  //   '(mouseenter)'       : "onmouseEnter123()",
+  //   '(mouseleave)'       : "onmouseleave345()",
+  //   '(click)'            : "onclick789()"
+  // }
 })
+
+
+
+// @hostbidning and @hostlistenr
+// :host{
+//     font-size:25px;
+//     font-weight: bold;
+//     color:blue
+// }
 export class App {
+
+    
+   @HostBinding('style.color')  
+   fontcolor = 'blue';
+  
+   @HostBinding('style.font-weight') 
+   fWeight   = 'bold'
+
+   @HostBinding('style.font-size')
+   hostfontsize = '30px';
+
+    @HostListener('mouseenter')
+    onMouseEnter1234(){
+          this.fontcolor = 'green';
+          this.fWeight   = 'normal'
+    }
+
+    @HostListener('mouseleave')
+    onmouseleave345(){
+          this.fontcolor = 'blue';
+          this.fWeight = 'bold'
+    }
+
+    @HostListener('click')
+    onclick789(){
+        //  this.backgroundColor123444 = 'yellow'
+        //alert("test");
+        this.fontcolor = 'red';
+    }
   
 }
